cea6a49e0f699ae13c60be85a9e9fd8c9afb4a2e
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-05-24 11:15:52 -0300

    não deu certo

--- index.js
+++ index.js
@@ -5,34 +5,53 @@ var series = [
     ['Game Of Thrones', 73, 50],
     ['The Mandalorian', 16, 45],
 ]
-const botoes = document.querySelectorAll('button')
+const botoes = document.querySelector('button')
 
-for(var i = 0; i<botoes.length; i++){
- botoes[i].addEventListener('click', dado)
-}
-
+botoes.addEventListener('click', dado)
 
 function dado(){
- var para = document.querySelector('#resposta')
-
-   return compara(para.value)
+let nome = document.querySelector('#serie')
+let eps = document.querySelector('#epsVistos')
+chamar(nome.value, eps.value)
 }
 
-function compara(nome){
-    var nome
-    for(let i = 0; i<series.length;i++){
-        for(var j = 0; j < 1;j++){
-             if (nome == series[i][j]){
-                console.log(nome);
-            }
-            
-            
-        }
+function chamar(a, b){
+let nEps = parseInt(b)
+let index;
+    if(a == 'InuYasha' || 'inuyasha'){
+    index = 0
+
+    }else if(a == 'naruto' || 'Naturo'){
+    index = 1
+
+    }else if(a == 'SwordArt' || 'swordart' || 'Swordart'){
+    index = 2
+
+    }else if(a == 'Game Of Thrones' || 'game of thrones'){
+    index = 3   
+
     }
+    else if(a == 'The Mandaloria' || 'the mandalorian'){
+        index = 4  
+    }
+    mostrar(index , nEps)
 }
- 
+
+function mostrar(nIndex , epsVistos){
+
+let totalEsp = parseInt(series[nIndex][1])
+let horas, eps
+let duracao = parseInt(series[nIndex][2])
+duracao /= 60;
+eps = totalEsp - epsVistos
+horas = duracao * eps
+let show = document.querySelector('#mostrar')
+show.textContent = 'Ainda faltam' + horas + 'para terminar de assistir essa serie'
 
 
+
+
+}
 
 
 
